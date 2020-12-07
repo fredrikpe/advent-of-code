60c1bb367e4c7d0fe8bfd18bc773cd81f73024ce
Fredrik Ingebrigtsen <fredrik.ingebrigtsen@nav.no>
2020-12-07 16:04:03 +0100

    7 part 2 python

--- python/7.py
+++ python/7.py
@@ -1,7 +1,27 @@
-lines = []
+
+tree = {}
+inverse_tree = {}
 with open("../input/7.txt", "r") as inputfile:
     for line in inputfile:
-        lines.append(line)
+        words = line.split(" ")
+        if len(words) == 7 or len(words) < 2:
+            continue
+
+        root = words[0] + " " + words[1]
+        words = words[4:]
+        leafs = []
+        while True:
+            leafs.append((int(words[0]), words[1] + " " + words[2]))
+            if len(words[3:]) < 2:
+                break
+            words = words[4:]
+        for (n, leaf) in leafs:
+            if leaf in inverse_tree:
+                inverse_tree[leaf].append(root)
+            else:
+                inverse_tree[leaf] = [root]
+
+        tree[root] = leafs
 
 
 def part1():
@@ -11,11 +31,11 @@ def part1():
     while len(targets) > 0:
         new_targets = set()
 
-        for line in lines:
-            for target in targets:
-                if target in line[1:]:
-                    node = line.split("bags con")[0]
-                    new_targets.add(node)
+        for target in targets:
+            if target not in inverse_tree:
+                continue
+            leafs = inverse_tree[target]
+            new_targets.update([s for s in leafs])
 
         nodes.update(new_targets)
         targets = new_targets
@@ -23,4 +43,16 @@ def part1():
     return len(nodes)
 
 
+def part2():
+    return part2_inner(1, "shiny gold") - 1
+
+def part2_inner(m, target):
+    if target not in tree:
+        return m
+
+    leafs = tree[target]
+    return m + sum([(part2_inner(m * f, s)) for (f, s) in leafs])
+
+
 print(part1())
+print(part2())
