75a17d845e1b601d96430e3065e7075a1d56a2d0
Fredrik Ingebrigtsen <fredrik.ingebrigtsen@nav.no>
2020-12-07 17:58:23 +0100

    make 7 part 1 recursive

--- python/7.py
+++ python/7.py
@@ -1,6 +1,6 @@
 
-tree = {}
-inverse_tree = {}
+dag = {}
+inverse_dag = {}
 with open("../input/7.txt", "r") as inputfile:
     for line in inputfile:
         words = line.split(" ")
@@ -8,7 +8,7 @@ with open("../input/7.txt", "r") as inputfile:
             continue
 
         root = words[0] + " " + words[1]
-        tree[root] = []
+        dag[root] = []
 
         while len(words) > 5:
             words = words[4:]
@@ -16,42 +16,31 @@ with open("../input/7.txt", "r") as inputfile:
             count = int(words[0])
             leaf = words[1] + " " + words[2]
 
-            if leaf in inverse_tree:
-                inverse_tree[leaf].append(root)
+            if leaf in inverse_dag:
+                inverse_dag[leaf].append(root)
             else:
-                inverse_tree[leaf] = [root]
+                inverse_dag[leaf] = [root]
 
-            tree[root].append((count, leaf))
-
+            dag[root].append((count, leaf))
 
 def part1():
-    nodes = set()
-    targets = set(["shiny gold"])
+    return len(part1_inner("shiny gold")) - 1
 
-    while len(targets) > 0:
-        new_targets = set()
+def part1_inner(target):
+    if target not in inverse_dag:
+        return [target]
 
-        for target in targets:
-            if target not in inverse_tree:
-                continue
-            leafs = inverse_tree[target]
-            new_targets.update([s for s in leafs])
-
-        nodes.update(new_targets)
-        targets = new_targets
-
-    return len(nodes)
+    return set([target]).union(*(part1_inner(leaf) for leaf in inverse_dag[target]))
 
 
 def part2():
     return part2_inner(1, "shiny gold") - 1
 
 def part2_inner(m, target):
-    if target not in tree:
+    if target not in dag:
         return m
 
-    leafs = tree[target]
-    return m + sum([(part2_inner(m * f, s)) for (f, s) in leafs])
+    return m + sum([(part2_inner(m * f, s)) for (f, s) in dag[target]])
 
 
 print(part1())
