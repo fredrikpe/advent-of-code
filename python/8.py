7e4bd1f36f35ebc2fda8412866438079f689cd4e
Fredrik Ingebrigtsen <fredrik.ingebrigtsen@nav.no>
2020-12-08 10:22:33 +0100

    day 8 python

---
+++ python/8.py
@@ -0,0 +1,52 @@
+
+LINES = []
+with open("../input/8.txt", "r") as inputfile:
+    for line in inputfile:
+        LINES.append(line)
+
+def run(lines):
+    acc = 0
+    visited = []
+    index = 0
+
+    while index not in visited and index < len(lines):
+        visited.append(index)
+
+        ins = lines[index][:3]
+        arg = int(lines[index][4:])
+
+        if ins == "acc":
+            acc += arg
+            index += 1
+        elif ins == "jmp":
+            index += arg
+        else:
+            index += 1
+
+    return acc, visited, index >= len(lines), index
+
+def part1():
+    acc, _, _, _  = run(LINES)
+    print(acc)
+
+def part2():
+    _, v, _, _ = run(LINES)
+
+    for i in v[::-1]:
+        lines = LINES
+        if LINES[i][:3] == "jmp":
+            lines[i] = "nop" + LINES[i][3:]
+        elif LINES[i][:3] == "nop":
+            lines[i] = "jmp" + LINES[i][3:]
+
+        a, v, s, I = run(lines)
+        if s:
+            print(i)
+            print(a, s)
+        if v == 345:
+            print("asdf")
+            print(LINES[i])
+            print(a, s, I)
+
+part1()
+part2()
