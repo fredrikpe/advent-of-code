3bf0b90cea3cbd53e35cb46d938ceacae2fed8d6
Fredrik Ingebrigtsen <fredrik.ingebrigtsen@nav.no>
2020-12-09 20:04:25 +0100

    cleanup

--- coq/day9.v
+++ coq/day9.v
@@ -14,9 +14,6 @@ Fixpoint make_pairs l : list (N*N) :=
   | x :: xs => make_pairs_with x xs ++ make_pairs xs
   end.
 
-Compute make_pairs_with 3%N [1%N ; 2%N].
-Compute make_pairs [ 3%N ; 1%N ; 2%N].
-
 Fixpoint contains {X:Type} (f:X -> bool) (l:list X) : bool :=
   match l with
   | nil => false
@@ -29,9 +26,6 @@ Definition contains_by_pair (f:(N*N) -> bool) (l:list N) : bool :=
 Definition pair_sum p : N :=
   N.add (fst p) (snd p).
 
-Compute pair_sum (6%N, 7%N).
-
-
 Definition contains_pair_sum (n:N) (l:list N) : bool :=
   contains (fun p => N.eqb (pair_sum p) n)  (make_pairs l).
 
@@ -42,16 +36,12 @@ Fixpoint take {X:Type} (n:nat) (l:list X) : list X :=
   | x :: xs, S n' => x :: take n' xs
   end.
 
-Compute take 2 [1;2;3].
-
 Fixpoint slice {X:Type} (n m:nat) (l:list X) : list X :=
   match l, n, m with
   | nil, _, _ => nil
   | l', O, m => take (m + 1) l'
   | x :: xs, S n', m => slice n' m xs
   end.
-
-Compute slice 3 0 [1;2;3].
 
 Fixpoint rev {X:Type} (l:list X) : list X :=
   match l with
@@ -114,7 +104,7 @@ Proof.
   intros. reflexivity.
 Qed.
 
-Lemma filter_by_next_n_first_m_with_tautologi:
+Lemma filter_by_next_n_first_m_with_tautologi_is_take:
   forall (X:Type) (l: list X) (n m:nat),
   filter_by_next_n_first_m tautologi_1 n m l = take m l.
 Proof.
@@ -149,43 +139,11 @@ Definition part1 (slice_length:nat) input : option N :=
 Example test_part1: part1 5 test_input = Some 127%N.
 Proof. reflexivity. Qed.
 
-
-Theorem fasolt: forall x xs,
-  contains_pair_sum x xs = false ->
-  exists x, part1 (length (x::xs)) (x::xs) = Some x.
-Proof.
-Admitted.
-
 Fixpoint sum (l: list N) : N :=
   match l with
   | nil => 0%N
   | x :: xs => N.add x (sum xs)
   end.
-
-
-Fixpoint init {X:Type} (l: list X) : list X :=
-  match l with
-  | nil => nil
-  | x :: nil => nil
-  | x :: xs => x :: init xs
-  end.
-
-Fixpoint tail {X:Type} (l: list X) : list X :=
-  match l with
-  | nil => nil
-  | x :: xs => xs
-  end.
-
-Fixpoint take_while_helper {X:Type} (f:list X -> bool) (h l: list X) : list X :=
-  match l with
-  | nil => nil
-  | x :: xs => if f (app h [x])
-      then take_while_helper f (h ++ [x]) xs
-      else h
-  end.
-
-Definition take_while {X:Type} (f:list X -> bool) (l: list X) : list X :=
-  take_while_helper f [] l.
 
 Fixpoint sublists_by_helper {X:Type} (f:list X -> bool) (h l:list X)
   : list (list X) :=
@@ -202,29 +160,29 @@ Fixpoint sublists_by {X:Type} (f:list X -> bool) (l:list X) : list (list X) :=
   | x :: xs => sublists_by_helper f [x] xs ++ sublists_by f xs
   end.
 
-Fixpoint max_elem_helper (n:option N) (l:list N) : option N :=
+Fixpoint _helper_max_elem (n:option N) (l:list N) : option N :=
   match l, n with
   | nil, _ => n
-  | x :: xs, None => max_elem_helper (Some x) xs
+  | x :: xs, None => _helper_max_elem (Some x) xs
   | x :: xs, Some y => if N.ltb x y
-        then max_elem_helper (Some y) xs
-        else max_elem_helper (Some x) xs
+        then _helper_max_elem (Some y) xs
+        else _helper_max_elem (Some x) xs
   end.
 
 Fixpoint max_elem (l:list N) : option N :=
-  max_elem_helper None l.
+  _helper_max_elem None l.
 
-Fixpoint min_elem_helper (n:option N) (l:list N) : option N :=
+Fixpoint _helper_min_elem (n:option N) (l:list N) : option N :=
   match l, n with
   | nil, _ => n
-  | x :: xs, None => max_elem_helper (Some x) xs
+  | x :: xs, None => _helper_min_elem (Some x) xs
   | x :: xs, Some y => if N.ltb x y
-        then min_elem_helper (Some x) xs
-        else min_elem_helper (Some y) xs
+        then _helper_min_elem (Some x) xs
+        else _helper_min_elem (Some y) xs
   end.
 
 Fixpoint min_elem (l:list N) : option N :=
-  min_elem_helper None l.
+  _helper_min_elem None l.
 
 Definition add_min_max (l:list N) :=
   match max_elem l, min_elem l with
@@ -233,28 +191,29 @@ Definition add_min_max (l:list N) :=
   | Some max, Some min => Some (N.add max min)
   end.
 
-Definition map {X Y:Type} (opt:option X) (f:X -> Y) : option Y :=
+Definition map {X Y:Type} (f:X -> Y) (opt:option X) : option Y :=
   match opt with
   | None => None
   | Some x => Some (f x)
   end.
 
-Definition flat_map {X Y:Type} (opt:option X) (f:X -> option Y) : option Y :=
+Definition flat_map {X Y:Type} (f:X -> option Y) (opt:option X) : option Y :=
   match opt with
   | None => None
   | Some x => f x
   end.
 
+Definition compose {X Y Z:Type} (g: Y -> Z) (h: X -> Y) :=
+  fun x => g (h x).
+
+Notation "g · f" := (compose g f) (at level 70, only parsing).
+
 Definition part2 (n:nat) (input:list N) :=
-  flat_map
-    (flat_map
-      (map
-          (part1 n input)
-          (fun x => sublists_by (fun l => N.eqb (sum l) x) input)
-      )
-      head
+  (flat_map add_min_max ·
+    (flat_map head ·
+      map (fun x => sublists_by (fun l => N.eqb (sum l) x) input)
     )
-    add_min_max.
+  ) (part1 n input).
 
 Compute part1 25 input.
 Compute part2 25 input.
